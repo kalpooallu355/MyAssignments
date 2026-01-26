87ce69c37b23a1660c8685ad830feb381b006bbd
kalpooallu355 <kalpooallu1@gmail.com>
2026-01-26 12:29:44 +0530

    Add files via upload
    week1assignment

---
+++ week1assignment/numberType.js
@@ -0,0 +1,13 @@
+let Numbers = 6
+
+if (Numbers >0){
+    console.log("Positive number")
+}
+
+else if(Numbers < 0){
+    console.log("Negative number")
+}
+
+if(Numbers ==0){
+    console.log("Number is zero")
+}
