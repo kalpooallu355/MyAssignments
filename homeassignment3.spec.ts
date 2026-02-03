f3027a3b6cf2ecc0d47674998d06221ec2cf30b2
kalpooallu355 <kalpooallu1@gmail.com>
2026-02-03 18:18:35 +0530

    Add files via upload
    yesterday's class assignment for CSS selector

---
+++ homeassignment3.spec.ts
@@ -0,0 +1,13 @@
+import test from '@playwright/test'
+
+test('open salesforce', async ({ page }) => {
+  await page.goto('https://login.salesforce.com/?locale=in')
+    await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com');
+await page.locator('#password').fill('TestLeaf@2025');
+await page.locator('#Login').click();
+await page.waitForTimeout(12000)
+await page.locator('button[title="App Launcher"]').click();
+await page.waitForTimeout(12000)
+await page.close();
+
+})
